1a6fa8f839c099e261fab2884146074b1c16126e
C-Perfume <akdl1020@gmail.com>
2021-04-13 17:53:24 +0900

    update Player, Enemy2.cs

--- Assets/Star/Scripts/Enemy2.cs
+++ Assets/Star/Scripts/Enemy2.cs
@@ -4,11 +4,39 @@ using UnityEngine;
 
 public class Enemy2 : MonoBehaviour
 {
+    public int speed = 5;
     public float destroyT = 5;
+    bool isDrop = true;
     // Start is called before the first frame update
     void Start()
     {
        Destroy(gameObject, destroyT);
         destroyT = Time.deltaTime;
+      
+    }
+    private void Update()
+    {
+        if(isDrop)
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+            if(transform.position.y <= 1)
+            {
+                isDrop = false;
+                transform.position = new Vector3(transform.position.x, 1, transform.position.z);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name.Contains("Spawn"))
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.constraints = RigidbodyConstraints.None;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
     }
 }
--- Assets/Star/Scripts/PlayerM.cs
+++ Assets/Star/Scripts/PlayerM.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class PlayerM : MonoBehaviour
 {
 
@@ -15,26 +17,41 @@ public class PlayerM : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //사용자의 입력을 받아
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
 
-        //전후좌우 방향으로
-        Vector3 dirH = Vector3.right * h;
-        Vector3 dirV = Vector3.forward * v;
+        if(Input.GetKeyDown(KeyCode.D))
+        {        
+            transform.position += Vector3.right;            
+            transform.eulerAngles = new Vector3(0, 90, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += Vector3.left;
+
+        }
+
         
 
-        //1의 값이 변해야 하는데..
-        //키를 뗄때만 값이 달라진다.
-        if (Input.GetButtonDown("Horizontal"))
-        {
-            //계속이동
-            transform.position += dirH * speed * Time.deltaTime;
-        }
-        else if (Input.GetButtonDown("Vertical"))
-        {
-            transform.position += dirV * speed * Time.deltaTime;
-        }
+        ////사용자의 입력을 받아
+        //float h = Input.GetAxis("Horizontal");
+        //float v = Input.GetAxis("Vertical");
+
+        ////전후좌우 방향으로
+        //Vector3 dirH = Vector3.right * h;
+        //Vector3 dirV = Vector3.forward * v;
+        
+
+        ////1의 값이 변해야 하는데..
+        ////키를 뗄때만 값이 달라진다.
+        //if (Input.GetButtonDown("Horizontal"))
+        //{
+        //    //계속이동
+        //    transform.position += dirH * speed * Time.deltaTime;
+        //}
+        //else if (Input.GetButtonDown("Vertical"))
+        //{
+        //    transform.position += dirV * speed * Time.deltaTime;
+        //}
       
     }
 }
