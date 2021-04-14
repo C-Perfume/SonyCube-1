68a32047085106fd4bf51b4b03ad834701108367
Hase20 <bluedoor03@gmail.com>
2021-04-14 17:47:29 +0900

    Update

--- Assets/Hasel/Scripts/MoveBlockEnemyX.cs
+++ Assets/Hasel/Scripts/MoveBlockEnemyX.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class MoveBlockEnemyX : MonoBehaviour
 {
-    //일정시간
-    float thisTime = 2;
     //흐르는시간
     float currTime = 0;
-
-    //적 이동속도
-    public float MoveSpeed = 4;
-    Vector3 dir;
-
     //이동가능
     bool movable = true;
 
@@ -29,12 +22,12 @@ public class MoveBlockEnemyX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(movable)
+        if (movable)
         {
             currTime += Time.deltaTime;
-            transform.position += transform.right *  Time.deltaTime * (1 / time);
+            transform.position += transform.right * Time.deltaTime * (1 / time);
             cube.transform.Rotate(0, 0, -90 * Time.deltaTime * (1 / time));
-            if(moveCnt >= 9)
+            if (moveCnt >= 10)
             {
                 transform.position -= transform.up * Time.deltaTime;
             }
@@ -50,40 +43,12 @@ public class MoveBlockEnemyX : MonoBehaviour
         {
             currTime += Time.deltaTime;
 
-            if(currTime > stayTime)
+            if (currTime > stayTime)
             {
                 movable = true;
                 currTime = 0;
             }
         }
-
-        //Destroy(gameObject, 15);
+        Destroy(gameObject, 18);
     }
-    /*
-    void EnemyMove()
-    {
-        if (thisTime <= currTime)
-        {
-            movable = true;
-        }
-    }
-    void trueMove()
-    {
-        if (movable)
-        {
-            transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
-            transform.eulerAngles += new Vector3(0, 0, -90) * Time.deltaTime;
-            currTime = 0;
-        }
-        else
-        {
-            movable = false;
-            transform.position = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
-        }
-    } /*
-
-    /* void NormalMove()
-    {
-        transform.position += transform.TransformDirection(Vector3.left) * MoveSpeed * Time.deltaTime;
-    }*/
 }
