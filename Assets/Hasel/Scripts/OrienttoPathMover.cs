ab54c3fea63db768817f88b6858477c7d94dc8f7
Hase20 <bluedoor03@gmail.com>
2021-04-26 18:04:06 +0900

    asd

--- Assets/Hasel/Scripts/OrienttoPathMover.cs
+++ Assets/Hasel/Scripts/OrienttoPathMover.cs
@@ -5,26 +5,45 @@ using UnityEngine;
 public class OrienttoPathMover : MonoBehaviour
 {
     [SerializeField]
-    Transform[] path;
+    public Transform[] path1;
+    public Transform[] path2;
+    float dealyT = 1.5f;
 
     private void Start()
     {
-        Move();
+        Move1();
     }
 
     void OnDrawGizmos()
     {
-        iTween.DrawPath(path);
+        iTween.DrawPath(path1);
+        iTween.DrawPath(path2);
     }
 
-    void Move()
+    void Move1()
     {
         iTween.MoveTo(gameObject, iTween.Hash(
-            "path", path, 
-            "time", 3, 
+            "path", path1, 
+            "time", 5, 
+            "delay", dealyT,
             "orienttopath", true, 
             "looktime", .6, 
-            "easetype", iTween.EaseType.easeInOutSine
+            "easetype", iTween.EaseType.easeInOutSine,
+            "oncomplete", "Move2"
+            //,"looptype", iTween.LoopType.loop
             ));
     }
+    void Move2()
+    {
+        iTween.MoveTo(gameObject, iTween.Hash(
+            "path", path2,
+            "time", 3,
+            //"delay", dealyT,
+            "orienttopath", true,
+            "looktime", .6,
+            "easetype", iTween.EaseType.easeInOutSine,
+            "oncomplete", "Move1"
+            ));
+    }
+
 }
--- Assets/Hasel/Scripts/TimeManager.cs
+++ Assets/Hasel/Scripts/TimeManager.cs
@@ -5,14 +5,22 @@ using UnityEngine.UI;
 
 public class TimeManager : MonoBehaviour
 {
-    public Text timeText;
+    public Text TimeText;
+    public Text CurrentScoreUI;
+    public Text BestScoreUI;
     float Sec;
     int Min;
     int Hour;
+    int bestScore;
+    float currScore;
+    int cMin;
+    int cHour;
 
     private void Start()
     {
         Sec = 55;
+        bestScore = PlayerPrefs.GetInt("BT");
+        BestScoreUI.text = "최대 점수 : " + bestScore;
     }
     private void Update()
     {
@@ -22,7 +30,10 @@ public class TimeManager : MonoBehaviour
     void TimeFlow()
     {
         Sec += Time.deltaTime;
-        timeText.text = string.Format("{0:D2}:{1:D2}:{2:D2}", Hour, Min, (int)Sec);
+        currScore += Time.deltaTime;
+        CurrentScoreUI.text = "현재 점수 : " + Mathf.Round(currScore).ToString();
+        TimeText.text = string.Format("{0:D2}:{1:D2}:{2:D2}", Hour, Min, (int)Sec);        
+
         if ((int)Sec > 59)
         {
             Sec = 0;
@@ -33,5 +44,16 @@ public class TimeManager : MonoBehaviour
             Min = 0;
             Hour++;
         }
+
+        if (currScore > bestScore)
+        {
+            SetBestTime((int)(currScore)); //베스트 점수 저장
+            PlayerPrefs.SetInt("BT", bestScore);
+        }
+    }
+    void SetBestTime(int BS)
+    {
+        bestScore = BS; //베스트 점수를 현재 점수로 갱신        
+        BestScoreUI.text = "최대 점수 : " + bestScore; //베스트 점수 UI 갱신
     }
 }
